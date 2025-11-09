6a72f9ac7ed74214cc446c35989f596c08080405
Csaba <n.csabi1977@gmail.com>
2025-11-09 11:57:53 +0100

    add booking class

---
+++ hotel-booking-system/src/main/java/org/example/model/Booking.java
@@ -0,0 +1,47 @@
+package org.example.model;
+
+public class Booking {
+    private Guest guest;
+    private Room room;
+    private int nights;
+    private MonetaryAmount totalPrice;
+
+    public Booking(Guest guest, Room room, int nights, MonetaryAmount totalPrice) {
+        this.guest = guest;
+        this.room = room;
+        this.nights = nights;
+        this.totalPrice = totalPrice;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public MonetaryAmount getTotalPrice() {
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Booking [Guest: %s, Room: %s, Nights: %d, Total Price: %s]",
+                guest.getName(),
+                room.getRoomNumber(),
+                nights,
+                totalPrice
+        );
+    }
+
+    int calculateTotalPrice() {
+        return room.getPricePerNight() * nights;
+    }
+}
+
