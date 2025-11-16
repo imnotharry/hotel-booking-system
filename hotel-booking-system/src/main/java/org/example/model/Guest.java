e789fa87fa93b6d27aa373eb8c01ac6065f936ec
CsoreTamas <csore.tamas123@gmail.com>
2025-11-16 09:50:42 +0100

    HOTEL-1: add lombok dependency

--- hotel-booking-system/src/main/java/org/example/model/Guest.java
+++ hotel-booking-system/src/main/java/org/example/model/Guest.java
@@ -1,11 +1,20 @@
 package org.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Guest {
+    private int id;
     private String name;
-
-
-    public String getName() {
-        return name;
-    }
+    private int age;
+    private MonetaryAmount balance;
+    private List<Booking> bookings;
 
 }
